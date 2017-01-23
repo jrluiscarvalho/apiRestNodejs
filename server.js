8b4e5706cabfd36c2608015418928edef47fb1df
Junior <lsco@gft.com>
2017-01-23 16:14:15 -0200

    config primary routes

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port = 3000;
+
+app.listen(port);
+
+
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
+
+console.log('Servidor http esta escutando na porta ' + port);
+
+app.get('/', function(req, res){
+    res.send({msg: 'teste'});
+});
+
+ 
+
+app.post('/api', function(req, res){
+    var dados = req.body;
+    
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        })
+    });
+    
+    res.send(dados);
+});
